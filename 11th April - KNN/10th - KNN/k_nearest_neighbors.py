3487e49792aa4a57b844fe667c940bba435638d5
Vishnu Vardhan <149938216+vishnukota05@users.noreply.github.com>
2025-05-15 22:02:29 +0530

    Add files via upload

---
+++ 11th April - KNN/10th - KNN/k_nearest_neighbors.py
@@ -0,0 +1,54 @@
+# K-Nearest Neighbors (K-NN)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+dataset = pd.read_csv(r"C:\Users\A3MAX SOFTWARE TECH\Desktop\WORK\1. KODI WORK\1. NARESH\2. EVENING BATCH\N_Batch -- 7.00PM -- JUL25\4. APR\10th - KNN\Social_Network_Ads.csv")
+
+#dataset = pd.read_csv(r'C:\Users\A3MAX SOFTWARE TECH\Desktop\WORK\1. KODI WORK\1. NARESH\2. EVENING BATCH\N_Batch -- 6.00PM\3. Jan'24\4th\Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20,random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the K-NN model on the Training set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# This is to get the Models Accuracy 
+from sklearn.metrics import accuracy_score 
+ac = accuracy_score(y_test, y_pred)
+print(ac) 
+
+
+# This is to get the Classification Report
+from sklearn.metrics import classification_report
+cr = classification_report(y_test, y_pred)
+cr
+
+bias = classifier.score(X_train,y_train)
+bias
+
+variance = classifier.score(X_test,y_test)
+variance
